2a5d5a64bc58670b7c899ddf975e7b91c3b1d1b2
Segun Emmanuel <83952885+segungbadamosi@users.noreply.github.com>
2022-06-15 20:50:03 +0100

    Commit correction

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,11 +1,29 @@
 #include <stdio.h>
+/**
+ * main - Code begins
+ * Return: 0
+ */
 
 int main(void)
 {
+	int n;
+	int a[5];
+	int *p;
   int n;
   int a[5];
   int *p;
 
+	a[2] = 1024;
+	p = &n;
+ /*
+  * write your line of code here...
+  * Remember:
+  * - you are not allowed to use a
+  */
+	*(p + 5) = 98;
+ /* ...so that this prints 98\n */
+	printf("a[2] = %d\n", a[2]);
+	return (0);
   a[2] = 1024;
   p = &n;
   /*
@@ -21,5 +39,4 @@ int main(void)
   printf("a[2] = %d\n", a[2]);
   return (0);
 }
-
 
